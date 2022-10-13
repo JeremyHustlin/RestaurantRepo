6dab641c592dd09f377bb67bc4b32128373f726c
Jeremy Hustlin <82581901+JeremyHustlin@users.noreply.github.com>
2022-10-14 02:43:55 +0300

    Create Ingredient.cs
    .Net 4.7.2 ConsoleApp

---
+++ Ingredient.cs
@@ -0,0 +1,18 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp2
+{
+    public class Ingredient
+    {
+        public string Cascaval { get; set; } = "Cascaval";
+        public string Price_Cascaval { get; set; } = "15 lei";
+        public string Masline { get; set; } = "Masline";
+        public string Price_Masline { get; set; } = "15 lei";
+        public string Carne { get; set; } = "Carne";
+        public string Price_Carne { get; set; } = "15 lei";
+    }
+}
