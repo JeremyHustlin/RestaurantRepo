6d116ce8365f13510293b420860ffad43ef95e4f
Jeremy Hustlin <82581901+JeremyHustlin@users.noreply.github.com>
2022-10-14 02:44:49 +0300

    Create Dishes.cs
    .Net 4.7.2 ConsoleApp

---
+++ Dishes.cs
@@ -0,0 +1,23 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp2
+{
+    public class Dishes
+    {
+        public string Margarita { get; set; } = "Margarita";
+        public string Margarita_Price { get; set; } = "80lei";
+        public string Capricioasa { get; set; } = "Capricioasa";
+
+        public string Capricioasa_Price { get; set; } = "110 lei";
+        public string Rancho { get; set; } = "Rancho";
+        public string Rancho_Price { get; set; } = "100 lei";
+
+        public string Cheese { get; set; } = "Cheese";
+        public string Cheese_Price { get; set; } = "115 lei";
+
+    }
+}
