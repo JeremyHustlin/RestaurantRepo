bfe3fd601105c15140da7d1e9838afb444cf970b
Jeremy Hustlin <82581901+JeremyHustlin@users.noreply.github.com>
2022-10-14 02:45:35 +0300

    Create Cook.cs
    .Net 4.7.2 ConsoleApp

---
+++ Cook.cs
@@ -0,0 +1,17 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp2
+{
+    public class Cook
+    {
+        public string First_Cook { get; set; } = "Mihail Mihail";
+        public string Second_Cook { get; set; } = "Alexandru Melnic";
+        
+
+
+    }
+}
