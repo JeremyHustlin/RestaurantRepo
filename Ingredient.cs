12df9f0502e297f634b788bf0e65e9830cf4d6c2
Jeremy Hustlin <82581901+JeremyHustlin@users.noreply.github.com>
2022-10-14 03:38:50 +0300

    Update Ingredient.cs
    .Net 4.7.2 ConsoleApp

--- Ingredient.cs
+++ Ingredient.cs
@@ -9,10 +9,10 @@ namespace ConsoleApp2
     public class Ingredient
     {
         public string Cascaval { get; set; } = "Cascaval";
-        public string Price_Cascaval { get; set; } = "15 lei";
+        public int Price_Cascaval { get; set; } = 15;
         public string Masline { get; set; } = "Masline";
-        public string Price_Masline { get; set; } = "15 lei";
+        public int Price_Masline { get; set; } = 15;
         public string Carne { get; set; } = "Carne";
-        public string Price_Carne { get; set; } = "15 lei";
+        public int Price_Carne { get; set; } = 15;
     }
 }
